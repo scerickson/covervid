d680a467492bb3ffa52b426047d87e6a2d55e897
Stefan Erickson <stefan.sce@gmail.com>
2014-05-02 13:23:35 -0500

    Initial commit

---
+++ covervid.js
@@ -0,0 +1,47 @@
+jQuery.fn.extend({
+  coverVid: function(width, height) {
+
+    var $this = this;
+
+    $(window).on('resize load', function(){
+
+      // Get parent element height and width
+      var parentHeight = $this.parent().height();
+      var parentWidth = $this.parent().width();
+
+      // Get native video width and height
+      var nativeWidth = width;
+      var nativeHeight = height;
+
+      // Get the scale factors
+      var heightScaleFactor = parentHeight / nativeHeight;
+      var widthScaleFactor = parentWidth / nativeWidth;
+
+      // Set necessary styles to position video "center center"
+      $this.css({
+        'position': 'absolute',
+        'top': '50%',
+        'left': '50%',
+        '-webkit-transform': 'translate(-50%, -50%)',
+        '-moz-transform': 'translate(-50%, -50%)',
+        '-ms-transform': 'translate(-50%, -50%)',
+        '-o-transform': 'translate(-50%, -50%)',
+        'transform': 'translate(-50%, -50%)',
+      });
+
+      // Based on highest scale factor set width and height
+      if(widthScaleFactor > heightScaleFactor) {
+        $this.css({
+          'height': 'auto',
+          'width': parentWidth
+        });
+      } else {
+        $this.css({
+          'height': parentHeight,
+          'width': 'auto'
+        });
+      }
+      
+    });
+  }
+});
