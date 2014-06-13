654f1c6e0ef7571016d135181c67950b9643f8e3
Stefan Erickson <sc.erickson@icloud.com>
2014-06-13 10:32:14 -0500

    Added grunt config

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		clean: ["covervid.min.js"],
+
+		uglify: {
+			dist: {
+				files: {
+					'covervid.min.js': ['covervid.js']
+				}
+			}
+		},
+
+		watch: {
+			dist: {
+				files: 'covervid.js',
+				tasks: ['clean', 'uglify', 'watch']
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['clean', 'uglify', 'watch']);
+
+};
--- covervid.js
+++ covervid.js
@@ -1,53 +1,55 @@
 jQuery.fn.extend({
-coverVid: function(width, height) {
+    coverVid: function(width, height) {
 
-    $(document).ready(sizeVideo);
-    $(window).resize(sizeVideo);
+        // Call sizeVideo() on load and resize
+        $(document).ready(sizeVideo);
+        $(window).resize(sizeVideo);
 
-    var $this = this;
+        // Define the attached selector
+        var $this = this;
 
-    function sizeVideo() {
+        function sizeVideo() {
 
-        // Get parent element height and width
-        var parentHeight = $this.parent().height();
-        var parentWidth = $this.parent().width();
+            // Get parent element height and width
+            var parentHeight = $this.parent().height();
+            var parentWidth = $this.parent().width();
 
-        // Get native video width and height
-        var nativeWidth = width;
-        var nativeHeight = height;
+            // Get native video width and height
+            var nativeWidth = width;
+            var nativeHeight = height;
 
-        // Get the scale factors
-        var heightScaleFactor = parentHeight / nativeHeight;
-        var widthScaleFactor = parentWidth / nativeWidth;
+            // Get the scale factors
+            var heightScaleFactor = parentHeight / nativeHeight;
+            var widthScaleFactor = parentWidth / nativeWidth;
 
-        // Set necessary styles to position video "center center"
-        $this.css({
-            'position': 'absolute',
-            'top': '50%',
-            'left': '50%',
-            '-webkit-transform': 'translate(-50%, -50%)',
-            '-moz-transform': 'translate(-50%, -50%)',
-            '-ms-transform': 'translate(-50%, -50%)',
-            '-o-transform': 'translate(-50%, -50%)',
-            'transform': 'translate(-50%, -50%)',
-        });
-
-        // Set overflow hidden on parent element
-        $this.parent().css('overflow', 'hidden');
-
-        // Based on highest scale factor set width and height
-        if(widthScaleFactor > heightScaleFactor) {
+            // Set necessary styles to position video "center center"
             $this.css({
-                'height': 'auto',
-                'width': parentWidth
+                'position': 'absolute',
+                'top': '50%',
+                'left': '50%',
+                '-webkit-transform': 'translate(-50%, -50%)',
+                '-moz-transform': 'translate(-50%, -50%)',
+                '-ms-transform': 'translate(-50%, -50%)',
+                '-o-transform': 'translate(-50%, -50%)',
+                'transform': 'translate(-50%, -50%)',
             });
-        } else {
-            $this.css({
-                'height': parentHeight,
-                'width': 'auto'
-            });
+
+            // Set overflow hidden on parent element
+            $this.parent().css('overflow', 'hidden');
+
+            // Based on highest scale factor set width and height
+            if(widthScaleFactor > heightScaleFactor) {
+                $this.css({
+                    'height': 'auto',
+                    'width': parentWidth
+                });
+            } else {
+                $this.css({
+                    'height': parentHeight,
+                    'width': 'auto'
+                });
+            }
+
         }
-
     }
-}
 });
