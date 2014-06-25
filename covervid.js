1509f5210e49eea5802acb0ab0c2b5e7de77455a
Jon wolfe <jonathanwolfe5@gmail.com>
2014-06-25 14:55:49 -0400

    Finished conversion to native

--- covervid.js
+++ covervid.js
@@ -1,136 +1,136 @@
-jQuery.fn.extend({
-	coverVid: function (width, height) {
+var coverVid = function (elem, width, height) {
 
-		function addEventListener(el, eventName, handler) {
+	// call sizeVideo on load
+	if (window.jQuery) {
+		$(document).ready(sizeVideo);
+	} else {
+		// Mozilla, Opera and webkit nightlies currently support this event
+		if (document.addEventListener) {
+			document.addEventListener('DOMContentLoaded', sizeVideo);
+
+			// If IE event model is used
+		} else if (document.attachEvent) {
+			// ensure firing before onload,
+			// maybe late but safe also for iframes
+			document.attachEvent("onreadystatechange", function () {
+				if (document.readyState === "complete") {
+					document.detachEvent("onreadystatechange", arguments.callee);
+					sizeVideo();
+				}
+			});
+		} else {
+			// A fallback to window.onload, that will always work
 			if (el.addEventListener) {
-				el.addEventListener(eventName, handler);
+				el.addEventListener('load', sizeVideo);
 			} else {
-				el.attachEvent('on' + eventName, function () {
-					handler.call(el);
-				});
+				el.attachEvent('on' + 'load', sizeVideo);
 			}
 		}
-
-		// call sizeVideo on load
-		function startOnReady(callback) {
-			if (window.jQuery) {
-				$(document).ready(sizeVideo);
-			} else {
-				// Taken from jQuery source
-
-				// Mozilla, Opera and webkit nightlies currently support this event
-				if (document.addEventListener) {
-					// Use the handy event callback
-					document.addEventListener("DOMContentLoaded", function () {
-						document.removeEventListener("DOMContentLoaded", arguments.callee, false);
-						callback;
-					}, false);
-
-
-					// If IE event model is used
-				} else if (document.attachEvent) {
-					// ensure firing before onload,
-					// maybe late but safe also for iframes
-					document.attachEvent("onreadystatechange", function () {
-						if (document.readyState === "complete") {
-							document.detachEvent("onreadystatechange", arguments.callee);
-							callback;
-						}
-					});
-
-					// If IE and not an iframe
-					// continually check to see if the document is ready
-					if (document.documentElement.doScroll && window == window.top)(function () {
-						try {
-							// If IE is used, use the trick by Diego Perini
-							// http://javascript.nwbox.com/IEContentLoaded/
-							document.documentElement.doScroll("left");
-						} catch (error) {
-							setTimeout(arguments.callee, 0);
-							return;
-						}
-
-						callback;
-					})();
-				}
-
-				// A fallback to window.onload, that will always work
-				addEventListener(window, 'load', callback);
-			}
-		}
-		startOnReady(sizeVideo);
-
-		// call sizeVideo on resize
-		function restartOnResize(callback) {
-
-			function debounce(fn, delay) {
-				var timer = null;
-
-				return function () {
-					var context = this,
-						args = arguments;
-
-					clearTimeout(timer);
-
-					timer = setTimeout(function () {
-						fn.apply(context, args);
-					}, delay);
-				};
-			}
-
-			if (window.jQuery) {
-				$(window).resize(debounce(callback, 50));
-			} else {
-				addEventListener(window, 'resize', debounce(callback, 50));
-			}
-
-		}
-		restartOnResize(sizeVideo);
-
-		// Define the attached selector
-		var $this = this,
-			sizeVideo = function () {
-
-				// Get parent element height and width
-				var parentHeight = $this.parent().height();
-				var parentWidth = $this.parent().width();
-
-				// Get native video width and height
-				var nativeWidth = width;
-				var nativeHeight = height;
-
-				// Get the scale factors
-				var heightScaleFactor = parentHeight / nativeHeight;
-				var widthScaleFactor = parentWidth / nativeWidth;
-
-				// Set necessary styles to position video "center center"
-				$this.css({
-					'position': 'absolute',
-					'top': '50%',
-					'left': '50%',
-					'-webkit-transform': 'translate(-50%, -50%)',
-					'-moz-transform': 'translate(-50%, -50%)',
-					'-ms-transform': 'translate(-50%, -50%)',
-					'-o-transform': 'translate(-50%, -50%)',
-					'transform': 'translate(-50%, -50%)',
-				});
-
-				// Set overflow hidden on parent element
-				$this.parent().css('overflow', 'hidden');
-
-				// Based on highest scale factor set width and height
-				if (widthScaleFactor > heightScaleFactor) {
-					$this.css({
-						'height': 'auto',
-						'width': parentWidth
-					});
-				} else {
-					$this.css({
-						'height': parentHeight,
-						'width': 'auto'
-					});
-				}
-
-			}
 	}
-});
+
+	// call sizeVideo on resize
+	function restartOnResize(callback) {
+
+		function debounce(fn, delay) {
+			var timer = null;
+
+			return function () {
+				var context = this,
+					args = arguments;
+
+				clearTimeout(timer);
+
+				timer = setTimeout(function () {
+					fn.apply(context, args);
+				}, delay);
+			};
+		}
+
+		if (window.jQuery) {
+			$(window).resize(debounce(callback, 50));
+		} else {
+			addEventListener(window, 'resize', debounce(callback, 50));
+		}
+
+	}
+	restartOnResize(sizeVideo);
+
+	// Define the attached selector
+	function sizeVideo() {
+
+		// Get parent element height and width
+		var parentHeight = (window.jQuery) ? elem.parent().height() : elem.parentNode.offsetHeight;
+		var parentWidth = (window.jQuery) ? elem.parent().width() : elem.parentNode.offsetWidth;
+
+		// Get native video width and height
+		var nativeWidth = width;
+		var nativeHeight = height;
+
+		// Get the scale factors
+		var heightScaleFactor = parentHeight / nativeHeight;
+		var widthScaleFactor = parentWidth / nativeWidth;
+
+		// Set necessary styles to position video "center center"
+		if (window.jQuery) {
+			elem.css({
+				'position': 'absolute',
+				'top': '50%',
+				'left': '50%',
+				'-webkit-transform': 'translate(-50%, -50%)',
+				'-moz-transform': 'translate(-50%, -50%)',
+				'-ms-transform': 'translate(-50%, -50%)',
+				'-o-transform': 'translate(-50%, -50%)',
+				'transform': 'translate(-50%, -50%)',
+			});
+		} else {
+			elem.style.position = 'absolute';
+			elem.style.top = '50%';
+			elem.style.left = '50%';
+			elem.style['-webkit-transform'] = 'translate(-50%, -50%)';
+			elem.style['-moz-transform'] = 'translate(-50%, -50%)';
+			elem.style['-ms-transform'] = 'translate(-50%, -50%)';
+			elem.style['-o-transform'] = 'translate(-50%, -50%)';
+			elem.style.transform = 'translate(-50%, -50%)';
+		}
+
+		// Set overflow hidden on parent element
+		if (window.jQuery) {
+			elem.parent().css('overflow', 'hidden');
+		} else {
+			elem.parentNode.style.overflow = 'hidden';
+		}
+
+		// Based on highest scale factor set width and height
+		if (widthScaleFactor > heightScaleFactor) {
+			if (window.jQuery) {
+				elem.css({
+					'height': 'auto',
+					'width': parentWidth
+				});
+			} else {
+				elem.offsetHeight = 'auto';
+				elem.offsetWidth = parentWidth;
+			}
+		} else {
+			if (window.jQuery) {
+				elem.css({
+					'height': parentHeight,
+					'width': 'auto'
+				});
+			} else {
+				elem.offsetHeight = parentHeight;
+				elem.offsetWidth = 'auto';
+			}
+		}
+
+	}
+};
+
+if (window.jQuery) {
+	jQuery.fn.extend({
+		'coverVid': function () {
+			coverVid(this, arguments[0], arguments[1]);
+			return this;
+		}
+	});
+}
